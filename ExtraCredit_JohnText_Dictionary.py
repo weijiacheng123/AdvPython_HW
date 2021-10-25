0e0733f7594d5f4a67a90e2b6e021dd8de4c1cc8
weijiacheng123 <weijia_cheng1@baylor.edu>
2021-10-24 19:50:44 -0500

    Extra Credit_JohnText

--- ExtraCredit_JohnText_Dictionary.py
+++ ExtraCredit_JohnText_Dictionary.py
@@ -1,19 +1,15 @@
 text = open('book_of_John_text.txt','r')
-d = dict()
-#d = { 1:'Father', 2:'God', 3:'Christ', 4:'Spirit', 5:'life', 6:'man' }
+#d = dict()
+d = {'Father':0, 'God':0, 'Christ':0, 'Spirit':0, 'life':0, 'man':0}
 
 for line in text:
     line = line.strip()
-    line = line.lower()
     words = line.split(" ")
 
     for word in words:
+        #print(word)
         if word in d:
             d[word] += 1
-        else:
-            d[word] = 1
 
 for key in list(d.keys()):
     print(key, ":", d[key])
-
-    
